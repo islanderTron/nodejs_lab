4a74e78b0df6f1b2073cce682bf1cdb69c6ba977
Paul Uncangco <pju93@hotmail.com>
2017-07-12 23:28:54 -0400

    hello world nodejs

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,20 @@
-var http = require('http');
+// console.log('hellow world');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+// Step 1 - Import Required Module
+var http = require("http");
 
-myfirst.js
+// Step 2 - Create Server
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+
+   // Listen port # MUST BE DIFFERENT than what we use for our localhost port #
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
