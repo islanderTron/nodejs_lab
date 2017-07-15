2fec6a305a01ff981fa1bf91913cd49b2028353e
Paul Uncangco <pju93@hotmail.com>
2017-07-14 23:33:01 -0400

    Learnyounode: Baby Steps

--- program.js
+++ program.js
@@ -1,2 +1,11 @@
-console.log('HELLO WORLD');
+// HELLO WORLD
+// console.log('HELLO WORLD');
 
+// BABY STEPS
+var sum = 0;
+for(var i = 1; i<process.argv.length; i++){
+	if(i>1){
+		sum += (process.argv[i]);
+	}
+}
+console.log(sum);
